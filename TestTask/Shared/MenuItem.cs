9422e47adb53e54cdb3309fffd2ae91955fc29f2
Егор Ципровский <egor.ciprov@gmail.com>
2022-10-25 19:08:26 +0300

    Реализация menu

---
+++ TestTask/Shared/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿using Syncfusion.Blazor.Navigations;
+
+
+namespace TestTask.Shared
+{
+    
+    public class MenuItem
+    {
+        public string Text { get; set; }
+        public string Url { get; set; }
+
+       
+    }
+}
